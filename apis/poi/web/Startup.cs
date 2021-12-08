e476810401056b40d671cab0fbdcc56f0d588b21
talksoft999 <85479107+talksoft999@users.noreply.github.com>
2021-12-08 14:49:35 +0000

    Update Startup.cs

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -95,7 +95,7 @@ namespace poi
             var json = new JObject(
                 new JProperty("message", "POI Service Healthcheck"),
                 new JProperty("status", result.Status.ToString()),
-                new JProperty("version", "v2"));
+                new JProperty("version", "v3"));
 
             return context.Response.WriteAsync(
                 json.ToString(Formatting.Indented));
