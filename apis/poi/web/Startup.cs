17eb30a8131ce2b19024330f7fcdc6beb2397f54
Sudama Prasad <sudama90tripathi@gmail.com>
2021-12-09 17:13:14 +0530

    Update Startup.cs

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -95,7 +95,7 @@ namespace poi
             var json = new JObject(
                 new JProperty("message", "POI Service Healthcheck"),
                 new JProperty("status", result.Status.ToString()),
-                new JProperty("version", "v1.0"));
+                new JProperty("version", "v.1.0.0"));
 
             return context.Response.WriteAsync(
                 json.ToString(Formatting.Indented));
