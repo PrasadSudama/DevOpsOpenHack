e3a631f39568b577ebfcaa0d2fbc96b636aebb49
MANISH KUMAR <47206018+newfunda@users.noreply.github.com>
2021-12-09 15:42:41 +0530

    Update Startup.cs

--- apis/poi/web/Startup.cs
+++ apis/poi/web/Startup.cs
@@ -95,7 +95,7 @@ namespace poi
             var json = new JObject(
                 new JProperty("message", "POI Service Healthcheck"),
                 new JProperty("status", result.Status.ToString()),
-                new JProperty("version", "v7"));
+                new JProperty("version", "v8"));
 
             return context.Response.WriteAsync(
                 json.ToString(Formatting.Indented));
